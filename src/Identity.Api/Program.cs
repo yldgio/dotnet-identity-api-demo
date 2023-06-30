22a5cad763ab7c56dc071660cff281107893822d
giovanni ferrara <giovanni.ferrara@agictech.com>
2023-06-30 14:25:12 +0200

    feat: enabled beta openapi lib

--- src/Identity.Api/Program.cs
+++ src/Identity.Api/Program.cs
@@ -1,6 +1,8 @@
 using Identity.Api;
 using Identity.Api.Application;
 using Identity.Api.Infrastructure;
+using RendleLabs.OpenApi.Web;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseStaticOpenApi(Assembly.GetExecutingAssembly(), "Identity.Api.openapi.yml", new StaticOpenApiOptions
+        {
+            Version = 1,
+            UiPathPrefix = "swagger",
+            JsonPath = "swagger/v1/openapi.json",
+            YamlPath = "swagger/v1/openapi.yaml",
+            Redoc = {
+                Path = "swagger/v1/redocs"
+            },
+            Elements =
+            {
+                Path = "swagger/v1/docs"
+            }
+        });
+    }
     app.UseExceptionHandler(errorHandlingPath: "/error");
     if (app.Environment.IsProduction())
     {
